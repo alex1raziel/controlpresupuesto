91a1cdfcfc113a3c07314bbc2cf0d18bb09c7f9b
Juan José Alexander Aceituno Ramírez <juanjo_alex@hotmail.com>
2020-02-13 21:30:42 -0300

    archivos inciales

---
+++ build/precache-manifest.90bf8738c9ed62a53e39460a12c44e46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d1118ab7629cf371a3ba125d6679c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ac9d23537d17e2b1393",
+    "url": "/static/css/main.c20d4f27.chunk.css"
+  },
+  {
+    "revision": "b4b21f7c481f2def4617",
+    "url": "/static/js/2.7458a811.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7458a811.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac9d23537d17e2b1393",
+    "url": "/static/js/main.646c6b5d.chunk.js"
+  },
+  {
+    "revision": "7cd4fbe71853055c0dc3",
+    "url": "/static/js/runtime-main.a235a530.js"
+  }
+]);
---
+++ src/components/Pregunta.js
@@ -0,0 +1,58 @@
+import React, {Fragment,useState} from 'react';
+import PropTypes from 'prop-types';
+import Error from "./Error";
+
+const Pregunta = ({guardarPresupuesto,guardarRestante,actualizarPregunta}) => {
+    //definir state
+    const [cantidad, guardarCantidad] = useState(0);
+    const [error, guardarError] = useState(false);
+
+    //funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value,10));
+    };
+
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+        //validar
+        if(cantidad<1 || isNaN(cantidad)){
+            guardarError(true);
+            return;
+        }
+        //paso validacion
+        guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarRestante(cantidad);
+        actualizarPregunta(false);
+    };
+
+    //submit para definir el presupuesto
+
+    return (
+        <Fragment>
+            <h2>Colola tu presupuesto</h2>
+            {error ? <Error mensaje="El presupuesto es incorrecto" /> : null}
+            <form
+                onSubmit={agregarPresupuesto}
+            >
+                <input
+                    type="number"
+                    className="u-full-width"
+                    placeholder="Coloca tu presupuesto"
+                    onChange={definirPresupuesto}
+                />
+                <input
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir presupuesto"
+                />
+            </form>
+        </Fragment>
+    )
+};
+Pregunta.propTypes = {
+    guardarPresupuesto: PropTypes.func.isRequired,
+    guardarRestante: PropTypes.func.isRequired,
+    actualizarPregunta: PropTypes.func.isRequired,
+};
+export default Pregunta;
